f4e01a804ba5a969a9418bf14fe48632359a04cb
mparente88 <parente.molly@gmail.com>
2024-12-04 11:04:19 -0800

    more changes

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat
 
 # Define the home view function
 def home(request):
@@ -7,20 +9,26 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    # Alternatively...
+    # fields = ['name', 'breed', 'description', 'age']
+    success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
